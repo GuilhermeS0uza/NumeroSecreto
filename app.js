0e9a5a20dc7095f9ec886084d907bb24f9572c50
GuilhermeS0uza <guibsouza04@gmail.com>
2025-02-16 18:53:22 -0300

    Comentario linha 9

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    // Descomente abaixo para usar o voice feedback
     // responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
